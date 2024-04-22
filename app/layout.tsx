7fac7a893e82803558f0f4779e321e71b5eb997f
Viateur <akimanaviateur94@gmail.com>
2024-04-22 19:04:40 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import Header from '@/components/Header'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Viateur AKIMANA',
+  description: 'Viateur, AKIMANA, Viateur AKIMANA, portfolio, website, portfolio website',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}</body>
+    </html>
+  )
+}
