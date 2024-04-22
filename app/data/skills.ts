65dfe0b8d91ce8ed28640d6943436d97f25b7938
Viateur <akimanaviateur94@gmail.com>
2024-04-22 21:44:16 +0200

    Making the projects links user friendly

--- app/data/skills.ts
+++ app/data/skills.ts
@@ -63,28 +63,28 @@ const professionalSkills = [
 const projects = [
   {
     name: "Watchio",
-    image: "/twittify.png",
+    image: "/watchio.png",
     description:
       "Watchio is the app build using reactjs and works like youtube",
     github: "https://github.com/Viateur-akimana/watchio-project-in-reactjs",
   },
   {
     name: "Movie app",
-    image: "/sociala.png",
+    image: "/movie_app.jpeg",
     description:
       "This is the movie app where you can choose music and see artist songs",
     github: "https://github.com/Viateur-akimana/Movie-app",
   },
   {
     name: "MediConnect",
-    image: "/pixel-portal.png",
+    image: "/mediconnect.png",
     description:
       "This is mediconnect where this is my current project that i am working on it so, it is done by using nodejs for backend and flutter for frontend mobile ",
     github: "https://github.com/Viateur-akimana/Mediconnect",
   },
   {
     name: "Musify",
-    image: "/bidify.png",
+    image: "/musify.jpeg",
     description:
       "Musify this is a music app build using typescript and nextjs",
     github: "https://github.com/Viateur-akimana/Music-app",
